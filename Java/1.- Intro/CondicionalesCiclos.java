e5885ca27e15ffbcd743b35169a44e54f1e95e5d
Cynthia Castillo <ca.castilloe@gmail.com>
2026-06-02 17:32:07 -0600

    Switch

--- Java/1.- Intro/CondicionalesCiclos.java
+++ Java/1.- Intro/CondicionalesCiclos.java
@@ -36,6 +36,35 @@ public class CondicionalesCiclos {
         if(edad >= 18 || permisoPadres) {
             System.out.println("Puedes obtener tu licencia de manejo");
         }
+
+        int dia = 5;
+        switch(dia) {
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miércoles");
+                break;
+            case 4:
+                System.out.println("Jueves");
+                break;
+            case 5:
+                System.out.println("Viernes");
+                break;
+            case 6:
+                System.out.println("Sábado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println("¡Solo tenemos 7 días!");
+                break;
+        }
+
     }
 
 }
