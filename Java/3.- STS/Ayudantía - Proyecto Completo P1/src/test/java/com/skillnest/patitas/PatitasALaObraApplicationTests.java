7311e8f6940ac1986b9a337db146739782876d09
CintyaRam <cintya.ramirezbarriga@gmail.com>
2026-06-24 23:04:41 -0400

    Ayudantía - Proyecto Completo P3

---
+++ Java/3.- STS/Ayudantía - Proyecto Completo P1/src/test/java/com/skillnest/patitas/PatitasALaObraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillnest.patitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatitasALaObraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
